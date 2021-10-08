9346e83a22eae6be92306bc3089419cb7e0130df
M K V Kishore <36737425+kishoremkv@users.noreply.github.com>
2021-10-08 18:08:21 +0530

    Added: Product Service - Dependency Injection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,46 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     'selector': 'pm-products',
     'templateUrl':'./product-list.component.html',
-    'styleUrls':['./product-list.component.css']
+    'styleUrls':['./product-list.component.css'],
+    providers:[ProductService]
 })
 export class ProductListComponent implements OnInit
 {
+
+  constructor(private productService: ProductService)
+  {
+
+  }
     pageTitle = "Product List";
-    products: IProduct[]=[
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-          },
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          }
-    ];
+    products: IProduct[]=[];
     imageWidth = 50;
     imageMargin = 2;
     showImage = false;
@@ -74,7 +50,8 @@ export class ProductListComponent implements OnInit
     ngOnInit(): void
     {
       console.log("on init method");
-      this.listFilter = '';
+      this.products = this.productService.getProducts();
+      this.filteredProducts = this.products;
     }
     
     onRatingClicked(message: string)
