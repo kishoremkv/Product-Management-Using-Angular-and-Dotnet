1cc05064e452fb1bb407a2e1541056bd47177c1e
M K V Kishore <36737425+kishoremkv@users.noreply.github.com>
2021-10-10 11:10:48 +0530

    Injected: ActivatedRoute

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-product-detail',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private route: ActivatedRoute, private router: Router) { }
+  pageTitle="Product Detail"
+  product: IProduct | undefined;
   ngOnInit(): void {
+    console.log(this.route.snapshot);
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle+=`: ${id}`;
+  }
+
+  onBack()
+  {
+    this.router.navigate(['/products']);
   }
 
 }
