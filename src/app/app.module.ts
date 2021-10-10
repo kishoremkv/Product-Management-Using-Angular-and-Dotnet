497ff56cc3f7d87e10b125b6c5eb75651240d34a
M K V Kishore <36737425+kishoremkv@users.noreply.github.com>
2021-10-10 11:38:07 +0530

    Added: Guard for ProductDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StarComponent } from './shared/star.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
@@ -18,14 +19,18 @@ import { RouterModule } from '@angular/router';
     StarComponent,
     WelcomeComponent,
     ProductDetailComponent
-  ],
+ ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       {path:'products',component: ProductListComponent},
-      {path: 'product/:id', component: ProductDetailComponent},
+      {
+        path: 'product/:id',
+        component: ProductDetailComponent,
+        canActivate:[ProductDetailGuard]
+      },
       {path: 'welcome', component: WelcomeComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'welcome', pathMatch: 'full' }
