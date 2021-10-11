6251b2cc1f29ea6d501ea27f4734fb7ddc72598f
M K V Kishore <36737425+kishoremkv@users.noreply.github.com>
2021-10-11 14:17:10 +0530

    Moved: files to frontend folder

---
+++ frontend/src/app/shared/star.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnChanges, EventEmitter, Output } from "@angular/core";
+
+@Component(
+    {
+        selector:'pm-star',
+        templateUrl: './star.component.html',
+        styleUrls:['./star.component.css']
+    }
+)
+export class StarComponent implements OnChanges
+{
+    cropWidth: number = 75;
+    @Input() rating: number=0;
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
+    ngOnChanges(): void
+    {
+        this.cropWidth = this.rating*75/5;
+        console.log("On changes star");
+    }
+    onClick(): void
+    {
+        this.ratingClicked.emit(` with ${this.rating} rating was clicked!`)
+    }
+
+}
